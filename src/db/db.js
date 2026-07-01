46dc0f34f62491408a4c6e03c10ab32b30be33c3
Rashmi7205 <rashmiranjanbehera8260@gmail.com>
2026-07-01 11:32:19 +0530

    Added database schemas

---
+++ src/db/db.js
@@ -0,0 +1,13 @@
+import 'dotenv/config';
+import { Pool } from 'pg';
+import { drizzle } from 'drizzle-orm/node-postgres';
+
+if (!process.env.DATABASE_URL) {
+  throw new Error('DATABASE_URL is not defined');
+}
+
+export const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+});
+
+export const db = drizzle(pool);
