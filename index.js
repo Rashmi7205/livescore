9b51b9587cdf0ad6f57fd22dffdbdd7017cc9c7b
Rashmi7205 <rashmiranjanbehera8260@gmail.com>
2026-08-05 20:07:07 +0530

    Code fixes

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.get('/', (req, res) => {
   res.send('Hello from Express Server!');
 });
 
-app.use(securityMiddleware());
+app.use(securityMiddleware);
 
 app.use("/matches", matchRouter);
 
--- src/arcject.js
+++ src/arcject.js
@@ -2,21 +2,21 @@ import arcjet, { detectBot, shield, slidingWindow } from "@arcjet/node";
 
 
 const arcjetKey = process.env.ARCJET_KEY;
-const arcjetMode = process.env.ARCJET_MODE === "DRY_RUN"?"DRY_RUN":"LIVE";
+const arcjetMode = process.env.ARCJET_MODE === "DRY_RUN" ? "DRY_RUN" : "LIVE";
 
-if(!arcjetKey){
+if (!arcjetKey) {
   throw new Error('Arcject key environemnt variable missing');
 }
 
 export const httpArcjet = arcjetKey ?
   arcjet({
-    key:arcjetKey,
-    rules:[
-      shield({mode:arcjetMode}),
-      detectBot({mode:arcjetMode,allow:['CATEGORY:SEARCH_ENGINE',"CATEGORY:PREVIEW"]}),
-      slidingWindow({mode:arcjetMode,interval:'10s',max:50})
+    key: arcjetKey,
+    rules: [
+      shield({ mode: arcjetMode }),
+      detectBot({ mode: arcjetMode, allow: ['CATEGORY:SEARCH_ENGINE', "CATEGORY:PREVIEW"] }),
+      slidingWindow({ mode: arcjetMode, interval: '10s', max: 50 })
     ],
-  }):null;
+  }) : null;
 
 export const wsArcjet = arcjetKey ?
   arcjet({
@@ -28,26 +28,27 @@ export const wsArcjet = arcjetKey ?
     ],
   }) : null;
 
-export function securityMiddleware(){
-  return async (req,resizeBy,next)=>{
-    if(httpArcjet) return next();
+export async function securityMiddleware(req, res, next) {
+  if (!httpArcjet) return next();
 
-    try {
-      const decision = await httpArcjet.project(req);
-      if(decision.isDenied()){
-        if(decision.reason.isRateLimit()){
-          return res.status(503).json({
-            error:"Too many requests."
-          })
-          return res.status(403).json({error:'Forbidden'})
-        }
+  try {
+    const decision = await httpArcjet.protect(req);
+
+    if (decision.isDenied()) {
+      if (decision.reason.isRateLimit()) {
+        return res.status(429).json({
+          error: 'Too many requests.'
+        });
       }
-    } catch (error) {
-      console.log(error);
-      return res.status(503).json({
-        error:'Service Unavailble'
-      });
+
+      return res.status(403).json({ error: 'Forbidden' });
     }
-    next();
+
+    return next();
+  } catch (error) {
+    console.log(error);
+    return res.status(503).json({
+      error: 'Service Unavailble'
+    });
   }
 }
